a2c583996bef882f7a78db71f39feee3baf16b5b
BeepBoop <fidel.zarcoa@sfa.michoacan.gob.mx>
2026-04-17 16:13:41 -0600

    Desacoplando tablas

--- src/migrations/events.sql
+++ src/migrations/events.sql
@@ -1,10 +1,25 @@
+
+DROP TABLE IF EXISTS events.event_publication;
+
+
 CREATE SCHEMA IF NOT EXISTS events;
-CREATE TABLE IF NOT EXISTS events.event_publication (
+
+CREATE TABLE events.event_publication (
     id UUID NOT NULL,
     listener_id VARCHAR(512) NOT NULL,
     event_type VARCHAR(512) NOT NULL,
-    serialized_event VARCHAR(4000) NOT NULL,
+    serialized_event TEXT NOT NULL,
     publication_date TIMESTAMP WITH TIME ZONE NOT NULL,
     completion_date TIMESTAMP WITH TIME ZONE,
+    
+    -- ¡Aquí están las nuevas columnas que Modulith necesita!
+    status VARCHAR(255) NOT NULL,
+    completion_attempts INTEGER,
+    last_resubmission_date TIMESTAMP WITH TIME ZONE,
+    
     PRIMARY KEY (id)
 );
+
+-- 4. Índices para que los reintentos automáticos sean rapidísimos
+CREATE INDEX IF NOT EXISTS event_publication_by_completion_date_idx 
+ON events.event_publication (completion_date);
---
+++ src/migrations/users.sql
@@ -0,0 +1,10 @@
+-- Crear tabla users
+CREATE TABLE IF NOT EXISTS users (
+    id             SERIAL PRIMARY KEY, 
+    username       VARCHAR(100) NOT NULL, 
+    password       VARCHAR(100) NOT NULL,
+    created_at     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    last_active_at TIMESTAMP WITH TIME ZONE,
+    deleted_at     TIMESTAMP WITH TIME ZONE
+);
