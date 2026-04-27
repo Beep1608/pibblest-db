1ecc46a63ea159e8a3f00a1ad43998d3bfe07670
BeepBoop <fidel.zarcoa@sfa.michoacan.gob.mx>
2026-04-27 13:50:36 -0600

    Quité regions por tags y agregue tabla que relaciona a las stores con tags

---
+++ src/main/resources/changelog/owner-data/004-stores_tags.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS stores_tags(
+    tag_id BIGINT,
+    store_id BIGINT,
+    CONSTRAINT pk_store_tag PRIMARY KEY (store_id, tag_id),
+    CONSTRAINT fk_tag FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE RESTRICT,
+    CONSTRAINT fk_store FOREIGN KEY (store_id) REFERENCES stores(id) ON DELETE RESTRICT
+);
+
+
+CREATE INDEX idx_tag_search ON stores_tags (tag_id);
