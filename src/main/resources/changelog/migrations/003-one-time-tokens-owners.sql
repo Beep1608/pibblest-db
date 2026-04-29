76fa1ae7c5b3fd91f50f5daec7e22366a84d8b21
Fidel Zarco <fidel.zarco1608@gmail.com>
2026-04-28 21:46:48 -0600

    Modifiqué la tabla de tokens de verificación para nueva funcionalidad

--- src/main/resources/changelog/migrations/003-one-time-tokens-owners.sql
+++ src/main/resources/changelog/migrations/003-one-time-tokens-owners.sql
@@ -1,10 +1,23 @@
-CREATE TABLE IF NOT EXISTS identity.one_time_tokens_owners(
+CREATE TABLE IF NOT EXISTS identity.one_time_tokens_owners (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    token_value VARCHAR(255) NOT NULL,
-    used BOOLEAN DEFAULT FALSE  NOT NULL,
-    owner_id UUID NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    expires_at TIMESTAMP WITH TIME ZONE , 
-    CONSTRAINT fk_tokne_owner FOREIGN KEY(owner_id) REFERENCES identity.owners(id) ON DELETE RESTRICT
 
+    token_value VARCHAR(255) UNIQUE NOT NULL, 
+    
+    used BOOLEAN DEFAULT FALSE NOT NULL,
+    expired BOOLEAN DEFAULT FALSE NOT NULL,
+    owner_id UUID NOT NULL,
+    
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+
+    expires_at TIMESTAMP WITH TIME ZONE NOT NULL, 
+    
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    
+
+    CONSTRAINT fk_token_owner FOREIGN KEY(owner_id) REFERENCES identity.owners(id) ON DELETE RESTRICT
 );
+
+
+CREATE INDEX idx_tokens_owner_active 
+ON identity.one_time_tokens_owners (owner_id) 
+WHERE expired = false AND used = false;
