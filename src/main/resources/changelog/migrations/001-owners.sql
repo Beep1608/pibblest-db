0c05e67b7e93b27ff6e2511da22ac37e3a305fa4
BeepBoop <fidel.zarcoa@sfa.michoacan.gob.mx>
2026-06-01 16:44:04 -0600

    Cambios en owners para un mejor manejo de cod de org, contraseñas y otras cosas

--- src/main/resources/changelog/migrations/001-owners.sql
+++ src/main/resources/changelog/migrations/001-owners.sql
@@ -2,21 +2,19 @@ CREATE SCHEMA IF NOT EXISTS identity;
 
 CREATE TABLE IF NOT EXISTS identity.owners (
     id                UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    company            TEXT,
+    company           VARCHAR(100) NOT NULL UNIQUE,
     name              VARCHAR(100) NOT NULL,
     last_name         VARCHAR(100) NOT NULL,
     email             VARCHAR(255) UNIQUE NOT NULL,
-    verified_at       TIMESTAMP,
-    password          VARCHAR(255) NOT NULL,
-    organization_code VARCHAR(10) UNIQUE,
-    is_active         BOOLEAN DEFAULT TRUE,
+    verified_at       TIMESTAMP WITH TIME ZONE,
+    password          VARCHAR(72) NOT NULL,
+    organization_code VARCHAR(20) UNIQUE,
+    is_active         BOOLEAN NOT NULL DEFAULT FALSE,
     last_login        TIMESTAMP WITH TIME ZONE,
     schema_name       VARCHAR(63) UNIQUE,
     created_at        TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at        TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
-
-
 
 CREATE OR REPLACE FUNCTION identity.update_updated_at_column() 
 RETURNS TRIGGER AS $$ 
