075be7178a4d162de06e01a7a7e1328e8c6261f7
BeepBoop <fidel.zarcoa@sfa.michoacan.gob.mx>
2026-06-05 16:39:02 -0600

    Cambios sobre el modulo de ventas

--- src/main/resources/changelog/owner-data/007-sales.sql
+++ src/main/resources/changelog/owner-data/007-sales.sql
@@ -1,11 +1,13 @@
---TODO: Añadir la relacion con los empleados
 CREATE TABLE IF NOT EXISTS sales (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
     store_id BIGINT NOT NULL,
+    employee_id UUID NOT NULL, -- Relación con el empleado que realizó la venta
     total_amount NUMERIC (12,2) NOT NULL DEFAULT 0.00,
     status VARCHAR(50) NOT NULL DEFAULT 'COMPLETED',
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP WITH TIME ZONE, -- Agregado para borrado suave
-    CONSTRAINT fk_sales_store FOREIGN KEY (store_id) REFERENCES stores (id) ON DELETE RESTRICT
+    deleted_at TIMESTAMP WITH TIME ZONE, -- Borrado suave
+    
+    CONSTRAINT fk_sales_store FOREIGN KEY (store_id) REFERENCES stores (id) ON DELETE RESTRICT,
+    CONSTRAINT fk_sales_employee FOREIGN KEY (employee_id) REFERENCES employees (id) ON DELETE RESTRICT
 );
